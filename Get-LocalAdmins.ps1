1ed680c40b9740ee657d42feee23eca94dfa4570
Facundo Bouza <facundo.bouza@gmail.com>
2020-06-10 18:27:42 -0300

    Update Get-LocalAdmins.ps1
    Comments added

--- Get-LocalAdmins.ps1
+++ Get-LocalAdmins.ps1
@@ -1,4 +1,22 @@
 Function Get-LocalAdmins {
+    <#
+    .SYNOPSIS
+    Gets the members of the local administrators of the computer 
+    and outputs the result to a CSV file.
+    .PARAMETER Computers
+    Specifies the Computer names of devices to query
+    .INPUTS
+    System.String. Get-LocalAdmins can accept a string value to
+    determine the Computers parameter.
+    .EXAMPLE
+    Para buscar un único equipo: Get-LocalAdmins -Equipos Equipo1, Equipo2
+    .EXAMPLE
+    Para buscar por listado de equipos: Get-LocalAdmins -Computers (Get-Content -Path "$env:HOMEPATH\Desktop\computers.txt")
+    .EXAMPLE
+    Para darle formato a la salida: Get-LocalAdmins -Computers DC,SVR8 | Format-Table -AutoSize -Wrap
+    .EXAMPLE
+    Para exportarlo a un CSV: utilGet-LocalAdmins -Computers DC,SVR8 | Export-Csv -Path "$env:HOMEPATH\Desktop\LocalAdmin.csv" -NoTypeInformation
+    #>
         
         Param(
             [Parameter(Mandatory)]
@@ -20,7 +38,7 @@ Function Get-LocalAdmins {
         $list = new-object -TypeName System.Collections.ArrayList
         foreach ($equipo in $EquiposUp) {
             $admins = Get-WmiObject -Class win32_groupuser -ComputerName $equipo | 
-                Where-Object {$_.groupcomponent -like '*"Administradores"'} 
+                Where-Object {$_.groupcomponent -like '*"Administradores"'} #set Administrators if you have English Configuration on Domain
             $obj = New-Object -TypeName PSObject -Property @{
                 ComputerName = $equipo
                 LocalAdmins = $null
