0bbd07305cbbacc424807d37b11a994b47bab70e
Facundo Bouza <facundo.bouza@gmail.com>
2020-06-11 23:56:07 -0300

    Update GetBasicInfoAllADusers.ps1
    Comments added

--- GetBasicInfoAllADusers.ps1
+++ GetBasicInfoAllADusers.ps1
@@ -4,17 +4,22 @@ $AcsExp = Get-ADUser -Filter {Name -like "*"} -properties Name | select-object -
  {
 	$Enabled = Get-ADUser -Filter "Name -like '$user'" -properties Enabled | select-object -ExpandProperty Enabled
 	$PassExpira = Get-ADUser -Filter "Name -like '$user'" -properties PasswordNeverExpires | select-object -ExpandProperty PasswordNeverExpires
-  $prop="Name","DisplayName","UserPrincipalName","Title","Department"
-  $dep = Get-ADUser -Filter "Name -like '$user'" -properties EmployeeNumber | select-object -ExpandProperty EmployeeNumber
+  	
+	#Creating variable to export info
+	$prop="Name","DisplayName","UserPrincipalName","Title","Department"
+  	
+	#Get Employee Number
+	$dep = Get-ADUser -Filter "Name -like '$user'" -properties EmployeeNumber | select-object -ExpandProperty EmployeeNumber
 
+	#Chicking if user has employee number and the pass not expires
 	if ($Enabled -eq $true -and $PassExpira -eq $false -and -not ([string]::IsNullOrEmpty($dep)))
 		{
             Write-Output (Get-ADUser -Filter "Name -like '$user'" -properties $prop | 
             Select-Object -Property $prop | 
             ConvertTo-Csv -NoTypeInformation |     
             Select-Object -Skip 1)
-			#Write-Output (Get-ADUser -Filter "Name -like '$user'" -properties $prop | ConvertTo-Csv -NoTypeInformation)
+	    #Write-Output (Get-ADUser -Filter "Name -like '$user'" -properties $prop | ConvertTo-Csv -NoTypeInformation)
 
         }
 
- }#For
+ }
