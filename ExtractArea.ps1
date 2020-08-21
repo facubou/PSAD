27f7c0e01d9d4a5e0f61caa0b107bee2b76b901a
Facundo Bouza <facundo.bouza@gmail.com>
2020-08-21 11:44:52 -0300

    Create ExtractArea.ps1
    Extract area from AD user

---
+++ ExtractArea.ps1
@@ -0,0 +1,9 @@
+$Usuarios = get-content -path "C:\temp\users.txt.txt"
+
+ForEach ($user in $usuarios)
+{
+    $unico = (((Get-ADUser -Filter "Name -like '$user'" -Properties CanonicalName | select-object -ExpandProperty CanonicalName) -split "/") | select -Skip 1) -join ','
+    $parse = $unico.Substring(0, $unico.IndexOf(',')) -replace '_', ' '
+    $salida = ( Get-Culture ).TextInfo.ToTitleCase( $parse.ToLower() )
+    Write-Host "$user,$salida"
+}
