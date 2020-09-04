c06d7b3d99db6e9ea81d545076b5a6533e94c555
Facundo Bouza <facundo.bouza@gmail.com>
2020-09-04 11:15:05 -0300

    Create CheckPassChange.ps1

---
+++ CheckPassChange.ps1
@@ -0,0 +1,45 @@
+$AcsExp = Get-ADUser -Filter {Name -like "*"} -properties Name | select-object -ExpandProperty Name
+$FechaHoy = Get-Date
+#$FechaHoy = $FechaHoy.ToString("dd/MM/yyyy")
+
+$Fromusr = "yourmailaccoint@domain.com"
+$Destinatario = "destinationmailaccount@domain.com"
+$Sub = "Seteo de password"
+$anonUser = "anonymous"
+$anonPass = ConvertTo-SecureString "anonymous" -AsPlainText -Force
+$anonCred = New-Object System.Management.Automation.PSCredential($anonUser, $anonPass)
+$Smtpsvr = "192.168.1.40" #your smtp server
+
+ForEach ($user in $AcsExp)
+{
+    $Enabled = Get-ADUser -Filter "Name -like '$user'" -properties Enabled | select-object -ExpandProperty Enabled
+    $PassExpira = Get-ADUser -Filter "Name -like '$user'" -properties PasswordNeverExpires | select-object -ExpandProperty PasswordNeverExpires
+    $nroEmpleado = Get-ADUser -Filter "Name -like '$user'" -properties EmployeeNumber | select-object -ExpandProperty EmployeeNumber
+
+    if ($Enabled -eq $true -and $PassExpira -eq $false -and -not ([string]::IsNullOrEmpty($nroEmpleado)))
+    {
+        $FechaPasswordLastSet = Get-ADUser -Filter "Name -like '$user'" -properties PasswordLastSet | select-object -ExpandProperty PasswordLastSet
+        $FechaHoy_Resta30 = $FechaHoy.AddDays(-30)
+        
+        $prop="Name","DisplayName","UserPrincipalName"
+
+        if ($FechaHoy_Resta30 -ge $FechaPasswordLastSet -and $FechaPasswordLastSet -like "*2020*")
+            
+            {
+                $data = Get-ADUser -Filter "Name -like '$user'" -properties $prop | 
+                Select-Object -Property $prop | 
+                ConvertTo-Csv -NoTypeInformation |     
+                Select-Object -Skip 1
+                $lista = $lista + $data + "," + $FechaVencimientoToString + "`n"
+                $Contador = $Contador +1
+
+                Write-Output ("[#] User "+$User)
+                Write-Output ("[#] Last Pass Change "+$FechaPasswordLastSet)
+                Write-Output "`n"
+            }
+    }
+}
+Write-Output $Contador
+write $lista | Out-File export1.txt
+$export = 'export1.txt'
+Send-MailMessage -to $Destinatario -from $fromusr -subject "$sub " -body "Lista de usuarios con seteo de pass >= 30 dias" -Attachments $export -SmtpServer $Smtpsvr -Credential $anonCred
