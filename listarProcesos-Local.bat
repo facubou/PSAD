0da8191868c3b0a4424fd54b45e5c1c0256049be
Facundo Bouza <facundo.bouza@gmail.com>
2021-01-07 13:46:18 -0300

    Add files via upload

---
+++ listarProcesos-Local.bat
@@ -0,0 +1,15 @@
+@echo off
+
+echo Creando informe...
+
+NETSTAT -A > output.txt
+
+TASKLIST /FI "STATUS eq RUNNING" /V >> output.txt
+
+type C:\WINDOWS\system32\drivers\etc\hosts >> output.txt
+
+echo Se creo el archivo output.txt
+
+pause
+exit
+
